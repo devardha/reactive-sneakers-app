f4881135f1021c5041f1d5af28af72e3f4b5393e
dev-ardha <ardhaydhtm@gmail.com>
2021-03-17 17:35:53 +0700

    Setup redux for state management

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigation from './src/components/navigation/TabNavigation';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store';
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <TabNavigation />
-        </NavigationContainer>
+        <Provider store={store}>
+            <NavigationContainer>
+                <TabNavigation />
+            </NavigationContainer>
+        </Provider>
     );
 };
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,53 +2,9 @@ import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import MainBanner from '../components/home/MainBanner';
 import ProductList from '../components/products/ProductList';
+import {connect} from 'react-redux';
 
-const Home = ({navigation}) => {
-    const products = [
-        {
-            image:
-                'https://static.nike.com/a/images/c_limit,w_318,f_auto/t_product_v1/88d2422d-152d-43a3-97ce-bc88aad8830c/kyrie-7-ep-play-future-basketball-shoe-FFZfk3.jpg',
-            productName: "Kyrie 7 EP 'Play for the Future'",
-            productCategory: "Men's Shoe",
-            price: '1,979,000',
-        },
-        {
-            image:
-                'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a16929ed-fff6-4f62-9ecb-7df009579f2d/jordan-ma2-shoe-qw1Z6m.jpg',
-            productName: 'Jordan MA2',
-            productCategory: 'Shoe',
-            price: '1,799,000',
-        },
-        {
-            image:
-                'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/c52f00f0-09f2-4dec-91f8-c3c2996e49a5/kd13-ep-basketball-shoe-1VlPqk.jpg',
-            productName: 'KD13 EP',
-            productCategory: 'Basketball Shoe',
-            price: '2,279,000',
-        },
-        {
-            image:
-                'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/760a7f13-41f0-4fd3-88de-6bd720601761/react-art3mis-shoe-zl7jW0.jpg',
-            productName: 'Nike React Art3mis',
-            productCategory: "Women's Shoe",
-            price: '1,649,000',
-        },
-        {
-            image:
-                'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/9412ec16-3067-45f3-9715-e6242748f2ba/air-force-1-07-se-shoe-Zvf6pd.jpg',
-            productName: "Nike Air Force 1 '07 SE",
-            productCategory: "Women's Shoe",
-            price: '1,499,000',
-        },
-        {
-            image:
-                'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/d2aa6c50-74c7-4c07-a398-850e5d684600/jordan-air-max-200-xx-shoe-v2glvX.jpg',
-            productName: 'Jordan Air Max 200 XX',
-            productCategory: "Women's Shoe",
-            price: '1,799,000',
-        },
-    ];
-
+const Home = ({navigation, products}) => {
     return (
         <View>
             <ScrollView contentContainerStyle={styles.container}>
@@ -66,4 +22,11 @@ const styles = StyleSheet.create({
     },
 });
 
-export default Home;
+const mapStateToProps = state => {
+    console.log(state);
+    return {
+        products: state.products.products,
+    };
+};
+
+export default connect(mapStateToProps)(Home);
